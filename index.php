4b72742f17b2a3f1c84a0ed4ef37d723a91420e7
Henrique <henrique.moriggi@hotmail.com>
2022-08-31 12:38:47 -0300

    Aparece a url dos veículos

--- index.php
+++ index.php
@@ -7,20 +7,21 @@
         <?php
             $urlAPI = "https://swapi.dev/api/people";
             $resultado = json_decode(file_get_contents($urlAPI));
-            var_dump($resultado);
+            //var_dump($resultado);
 
             foreach ($resultado->results as $personagem) {
                 echo "Personagem: " . $personagem->name . "<br>";
                 echo "Altura: " . $personagem->height . " cm" . "<br>";
                 echo "Peso: " . $personagem->mass . " kg" . "<br>";
-                echo "Veículo: " . $personagem->vehicles . "<br>";
+                //echo "Veículo: " . $personagem->vehicles . "<br>";
 
-                /*$urlVeiculo = "https://swapi.dev/api/vehicles";
+                $urlVeiculo = "https://swapi.dev/api/vehicles";
                 $resultadoVeiculo = json_decode(file_get_contents($urlVeiculo));
-                var_dump($resultadoVeiculo);
+                //var_dump($resultadoVeiculo);
+
                 foreach ($personagem->vehicles as $veiculo) {
-                    echo "Veículo: " . $veiculo->name . "<br>";
-                }*/
+                    echo "Veículo: " . $veiculo . "<br>";
+                }
 
                 echo "<hr>";
             }
