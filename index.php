cd178b124a39516750dc6f458d311399fc87c0d4
Henrique <henrique.moriggi@hotmail.com>
2022-08-31 11:46:40 -0300

    Arquivos iniciais

---
+++ index.php
@@ -0,0 +1,29 @@
+<html>
+    <head>
+        <meta charset="UTF-8">
+        <title></title>
+    </head>
+    <body>
+        <?php
+            $urlAPI = "https://swapi.dev/api/people";
+            $resultado = json_decode(file_get_contents($urlAPI));
+            var_dump($resultado);
+
+            foreach ($resultado->results as $personagem) {
+                echo "Personagem: " . $personagem->name . "<br>";
+                echo "Altura: " . $personagem->height . " cm" . "<br>";
+                echo "Peso: " . $personagem->mass . " kg" . "<br>";
+                echo "Veículo: " . $personagem->vehicles . "<br>";
+
+                /*$urlVeiculo = "https://swapi.dev/api/vehicles";
+                $resultadoVeiculo = json_decode(file_get_contents($urlVeiculo));
+                var_dump($resultadoVeiculo);
+                foreach ($personagem->vehicles as $veiculo) {
+                    echo "Veículo: " . $veiculo->name . "<br>";
+                }*/
+
+                echo "<hr>";
+            }
+        ?>
+    </body>
+</html>
